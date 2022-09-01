a858639a688f5e5cf24bbb5d0497f5dbf9156f6c
AleksOdessa <aleksjavadev@gmail.com>
2022-09-01 14:28:55 +0300

    Add  and show data from array

---
+++ myscripts/arrayscript.js
@@ -0,0 +1,36 @@
+const animals = ["bee", "cow", "crab"];
+
+function showAnimal(arr, elem) {
+  let out = "";
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i]!== undefined){
+    out += `<div> <img src="img/${arr[i]}.png"><span>${i}</span></div>`;
+    }
+  }
+  document.querySelector(elem).innerHTML = out;
+  document.querySelector(".out-1-source-length").innerHTML =
+    "Длина массива " + animals.length;
+
+  document.querySelector(".out-1-source").innerHTML =
+    "const animal = [" + animals + "]";
+}
+
+showAnimal(animals, ".out-1-source-img");
+
+
+function addToArray() {
+    if(!Number.isInteger(document.querySelector(".array-index").value)){
+        alert('ТОлько целые числа')
+    }
+  let index = parseInt(document.querySelector(".array-index").value);
+  if(index>50) {
+    alert('Много!!!!!!!');
+  }
+  else if(index<0) {
+    alert('Отрицательно');
+  }
+  animals[index] = document.querySelector(".array-element").value;
+  showAnimal(animals, ".out-1-source-img");
+}
+
+document.querySelector(".add-to-array").onClick = addToArray();
---
+++ myscripts/nav.js
@@ -0,0 +1,10 @@
+let navig = document.createElement();
+
+function nav () {
+    return (<div class="out" style="margin-left: 20px; margin-bottom: 20px;">
+    <a href="filter.html">Filtr</a>
+    <a href="arrSelectAddRemove.html">Example Array</a>
+   </div>);
+};
+
+nav();
