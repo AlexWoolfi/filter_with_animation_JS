82b804f4adfe504b1cbe7ed57931e2c91185dfaa
AleksOdessa <aleksjavadev@gmail.com>
2022-09-11 13:25:33 +0300

    hash part2 done

--- myscripts/leafBloks.js
+++ myscripts/leafBloks.js
@@ -3,7 +3,7 @@ document.querySelector('.right').addEventListener('click',articleRight);
 
 let section = document.querySelectorAll('article section');
 let sectionCount = section.length; // Нужны проверки!!!!!!!!!!
-let hesh = 0;// # данные в строке браузера
+let hash = 0;// # данные в строке браузера
 
 //Проверяем есть ли хеш при первой загрузке
 if (window.location.hash) {
@@ -13,29 +13,31 @@ if (window.location.hash) {
 }
 
 function showSection() {
-    section.forEach( el => {
+    section.forEach(el => {
         el.classList.add('hide-section');
     });
     section[hash].classList.remove('hide-section');
 }
 
+//ЕСли значение хеш -1 больше равно нулю, он же старт отсчета. ТОгда кнопка работает
 function articleLeft () {
-  if(hash -1 >= 0) {
+  if (hash -1 >= 0) {
     hash--;
   }
   else {
     hash = sectionCount-1;
   }
-  hash = window.location.hash;
+  window.location.hash =  hash;
   showSection();
 }
-
+// Если увеличение хеша на 1 меньше длины всего массива, тогда правая кнопка может работать
 function articleRight () {
-    if(hash +1 <sectionCount) {
+    if(hash +1 < sectionCount) {
       hash++;
     }
     else {
       hash = 0;
-    }hash = window.location.hash;
+    }
+   window.location.hash =  hash;
     showSection();
   }
