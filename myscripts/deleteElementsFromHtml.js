0bf7ae5ee5c8c304a64a522da602d1a4d1e04a72
AleksOdessa <aleksjavadev@gmail.com>
2022-09-18 13:52:24 +0300

    udalualkaElementsFromHtml

---
+++ myscripts/deleteElementsFromHtml.js
@@ -0,0 +1,11 @@
+const iframe = document.querySelector('iframe, div.player');
+for(let i = 0; i < iframe.length; i++) {
+   iframe[i].remove();
+}
+document.onclick = removeNode;
+
+function removeNode(event) {
+   event.preventDefault();
+   event.target.remove();
+   return false;
+}
