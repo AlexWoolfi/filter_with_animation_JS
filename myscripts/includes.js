288d56f372711aa9ad2bd6163a3023720f38993d
AleksOdessa <aleksjavadev@gmail.com>
2022-10-14 12:32:13 +0300

    inclides

---
+++ myscripts/includes.js
@@ -0,0 +1,63 @@
+const transaction = ['57dfsgdsf23fdsf', 'df5566gdfgzvf','345frefe55gfgst',
+'1cjfvhe5700cdw','dfsfp00914cd'];
+
+const test = [44,55,66,77,88,2,3,4];
+
+// // #1
+// if(test.indexOf(5) !== -1) {
+//     console.log('yes');
+// }
+// else {
+//     console.log('no');
+// }
+
+// // #2
+// if(~test.indexOf(77)) {
+//     console.log('yes');
+// }
+// else {
+//     console.log('no');
+// }
+
+// // #3
+// if(test.indexOf(2) + 1) {
+//     console.log('yes');
+// }
+// else {
+//     console.log('no');
+// }
+
+// // #4 includes - return true or false
+// if(test.includes(908)) {
+//     console.log('yes includes');
+// }
+// else {
+//     console.log('no includes');
+// }
+
+// function incl(ar,a) {
+//     ar.includes(a) ? console.log('yes includes') : console.log('no includes');
+// }
+
+// incl(test,908);
+
+// // Метод инклюдс возвращает тру даже с частью символов
+// let str = 'qwertyu';
+// if(str.includes('qwer')) {
+//     console.log('yes includes');
+// }
+// else {
+//     console.log('no includes');
+// }
+
+const users = [
+    {'name' : "Ivan", 'age' : 44},
+    {'name' : "Petr", 'age' : 14},
+    {'name' : "Alex", 'age' : 37},
+    {'name' : "Alix", 'age' : 43}
+];
+
+// let newUsers = users.filter( item => item.name.toLowerCase().includes('al') 
+// || item.age.includes(44));
+
+console.log(newUsers);
