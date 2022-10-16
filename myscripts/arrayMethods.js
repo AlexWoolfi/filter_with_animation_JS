73ce49ae7c3b28044b610cf3fbb25370aaeabd53
AleksOdessa <aleksjavadev@gmail.com>
2022-10-16 13:04:26 +0300

    MethodsArrayUpdate

--- myscripts/arrayMethods.js
+++ myscripts/arrayMethods.js
@@ -1,48 +1,97 @@
-const odj = [
-    {
-        'name' : 'Serg',
-        'level' : 'hacer'
-    },
-    {
-        'name' : 'Ivan',
-        'level' : 'lamer'
-    },
-    {
-        'name' : 'Peth',
-        'level' : 'doomer'
-    },
-    {
-        'name' : 'Vas',
-        'level' : 'hacer'
-    }
-];
+// const odj = [
+//     {
+//         'name' : 'Serg',
+//         'level' : 'hacer'
+//     },
+//     {
+//         'name' : 'Ivan',
+//         'level' : 'lamer'
+//     },
+//     {
+//         'name' : 'Peth',
+//         'level' : 'doomer'
+//     },
+//     {
+//         'name' : 'Vas',
+//         'level' : 'hacer'
+//     }
+// ];
 
-//ЕСть ли в массиве обект ...
-let res = odj.some(item => {
-    return (item.level === 'doomer');
-});
+// //ЕСть ли в массиве обект ...
+// let res = odj.some(item => {
+//     return (item.level === 'doomer');
+// });
 
-console.log(res);
+// console.log(res);
 
-//Flag
-let flag = false;
-odj.forEach(element => {
-    if(element.level === 'doomer'){
-        flag = true;
-    }
-});
-console.log('Flag = '+ flag);
+// //Flag
+// let flag = false;
+// odj.forEach(element => {
+//     if(element.level === 'doomer'){
+//         flag = true;
+//     }
+// });
+// console.log('Flag = '+ flag);
 
-//Каждый ли обект ...
-let res2 = odj.every(item => {
-    return (item.level === 'hacer');
-});
+// //Каждый ли обект ...
+// let res2 = odj.every(item => {
+//     return (item.level === 'hacer');
+// });
 
-console.log(res2);
+// console.log(res2);
 
-//Возвращает первый встреченный элемент
-let res3 = odj.find(item => {
-    return (item.level === 'hacer');
-});
+// //Возвращает первый встреченный элемент
+// let res3 = odj.find(item => {
+//     return (item.level === 'hacer');
+// });
 
-console.log(res3);
+// console.log(res3);
+
+
+// // SHIFT - удвляет первый элемент мессива и возвращает его значение
+// let a = [3,4,5];
+
+// let b = a.shift();
+// console.log(b);
+// console.log(a);
+
+
+// // UNSHIFT - добавляет значение из параметров в первый элемент мессива и возвращает его длину
+// let a = [3,4,5];
+
+// let b = a.unshift(22);
+// console.log(b);
+// console.log(a);
+
+// //SlICE - создает новый массив, копируя последовательно исходный начиная с позиции из параметра
+// //может принимать второй параметр до которого, не включая. Работает со строками
+// let a = [3,4,5,6,7,8,9];
+
+// let b = a.slice(3,5);
+// console.log(a);
+// console.log(b);
+
+//CONCAT - Объединяет два масиива, возвращает новый массив
+
+// const a1 = [21,22,23,24];
+// const a2 = [31,32,33,34];
+// const a3 = [48,58,68];
+
+// const b = a1.concat(a2);
+// const c = a1.concat(a2,a3);
+// console.log(b);
+// console.log(c);
+
+// let d = "Hello";
+// let v = "hi";
+// let t = d.concat(v);
+// console.log(t);
+
+//SPLICE - удаляет изменяет содержимое массива и меняет исходный массив
+//1 - параметр, указываем позицию в массиве.
+//2 - параметр, сколько всего должно быть удалено, включая элемент с указанной позиции
+//3 - необязательный параметр, заменяет данные на первой позиции и удаляет вторую
+// let x = [33,44,55,66,77,88];
+// console.log(x);
+// x.splice(2,2,555);
+// console.log(x);
--- myscripts/dublicateFromArray.js
+++ myscripts/dublicateFromArray.js
@@ -1,35 +1,64 @@
-const arr = [1,2,3,4,1,5,6,1,7,2];
-console.log(arr);
-//1 Set - содержит только уникальные элементы
-let a1 = new Set(arr);// Это просто набор
-console.log(a1);
+// const arr = [1,2,3,4,1,5,6,1,7,2];
+// console.log(arr);
+// //1 Set - содержит только уникальные элементы
+// let a1 = new Set(arr);// Это просто набор
+// console.log(a1);
 
-let a2 = [...new Set(arr)];// 2 Получаем массив с спомощью spred
-console.log(a2);
+// let a2 = [...new Set(arr)];// 2 Получаем массив с спомощью spred
+// console.log(a2);
 
-let a3 = Array.from(new Set(arr));// 3 Получаем массив 
-console.log(a3);
+// let a3 = Array.from(new Set(arr));// 3 Получаем массив 
+// console.log(a3);
 
-// 4 Получаем массив с спомощью filtr, если нужны дубликаты то ставим !==
-let a4 = arr.filter((item, index) => {
-    return arr.indexOf(item) === index;
+// // 4 Получаем массив с спомощью filtr, если нужны дубликаты то ставим !==
+// let a4 = arr.filter((item, index) => {
+//     return arr.indexOf(item) === index;
 
-});
+// });
 
-console.log('Filtr = ' + a4);
+// console.log('Filtr = ' + a4);
 
-let a5 = arr.filter((item, index) => {
-    return arr.indexOf(item) !== index;
+// let a5 = arr.filter((item, index) => {
+//     return arr.indexOf(item) !== index;
 
-});
+// });
 
-console.log('Filtr = ' + a5);
+// console.log('Filtr = ' + a5);
 
-//5 reduce
+// //5 reduce
 
-let a6 = arr.reduce( (result, item) => {
-    return result.includes(item) ? result : [...result, item];
-}, []);
-console.log('Reduce = ' + a6);
+// let a6 = arr.reduce( (result, item) => {
+//     return result.includes(item) ? result : [...result, item];
+// }, []);
+// console.log('Reduce = ' + a6);
+
+// //Находим большее значение
+// let ab = [-3,4,-10,7,11,300,301];
+
+// let re = ab.reduce((accum,item) => {
+//     if(item > accum) {
+//         accum = item;
+//     }
+//     return accum;
+// });
+
+// console.log(re);
+
+//Создать массив из части объекта
+let obj2 = [
+    {'id':55, 'city': "arc"},
+    {'id':75, 'city': "rca"},
+    {'id':175, 'city': "bra"}
+
+];
+
+let res = obj2.reduce((accum,item) => {
+    accum.push(item.id);
+    return accum;
+},[]);
+console.log(res);
+
+console.log(Array.isArray(res));
+
 
 
