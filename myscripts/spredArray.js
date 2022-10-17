6d116b47fd51cd9c8685c070ff1b2803b8fec03f
AleksOdessa <aleksjavadev@gmail.com>
2022-10-17 11:11:45 +0300

    Spred

---
+++ myscripts/spredArray.js
@@ -0,0 +1,29 @@
+// const a = [3,4,5];
+// const b = ['c','d','e'];
+
+// //Объединенный merge
+// const c = [...a, ...b];
+// console.log(c);
+
+// //Clone array
+// const d = [...c];
+// console.log(d);
+
+// //Строка в массив string - array
+// const e = [..."hello"]
+// console.log(e);
+
+// //set - array(уникальные значения)
+// const k = [...new Set([1,1,2,3,4,2])];
+// console.log(k);
+
+// //В Функциях
+// function test() {
+//     console.log([...arguments]);
+// }
+// test(3,4,5,6);
+
+// node list(querySelectorAll())
+// let p = document.querySelectorAll('p');
+// console.dir(p);
+// console.log([...p]);
